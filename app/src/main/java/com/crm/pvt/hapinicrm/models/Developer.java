2f6a0482ea9aafd766ac031f4f83b36e6dd82c10
Rohan <rohankulk82@gmail.com>
2021-06-16 19:02:25 +0530

    Rohan 16 june 7:01pm

---
+++ app/src/main/java/com/crm/pvt/hapinicrm/models/Developer.java
@@ -0,0 +1,25 @@
+package com.crm.pvt.hapinicrm.models;
+
+public class Developer {
+    String number,password;
+
+    Developer(){
+
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
