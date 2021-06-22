d8d5e0a31adc5756c2795acdac9a239787592756
Rohan <rohankulk82@gmail.com>
2021-06-23 00:45:21 +0530

    22 night(2)

--- app/src/main/java/com/crm/pvt/hapinicrm/Employee_Active_user.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/Employee_Active_user.java
@@ -15,6 +15,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +39,8 @@ public class Employee_Active_user extends AppCompatActivity {
     private String orderby = "Name";
     private int count;
     TextView text;
+    private EditText inputtext;
+    ImageButton img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +55,12 @@ public class Employee_Active_user extends AppCompatActivity {
         empref = FirebaseDatabase.getInstance().getReference().child("Employee");
         query1 = empref.orderByChild("City");
         query2 = empref.orderByChild("Name");
+
+        inputtext = findViewById(R.id.searchtextE);
+        img = findViewById(R.id.searchbtnE);
+
+
+
 
 
 
@@ -87,6 +97,13 @@ public class Employee_Active_user extends AppCompatActivity {
         super.onStart();
         EmployeeFirebase();
 
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EmployeeFirebasesearch();
+            }
+        });
+
     }
 
     public void EmployeeFirebase() {
@@ -106,6 +123,82 @@ public class Employee_Active_user extends AppCompatActivity {
         FirebaseRecyclerOptions<Employee> empoptions =
                 new FirebaseRecyclerOptions.Builder<Employee>()
                         .setQuery(empref, Employee.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Employee, ActiveUserActivity.EmplistViewHolder> empadapter = new FirebaseRecyclerAdapter<Employee, ActiveUserActivity.EmplistViewHolder>(empoptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ActiveUserActivity.EmplistViewHolder holder, int position, @NonNull Employee model) {
+                holder.Username.setText("Name : "+model.getName());
+                holder.Passcode.setText("IMEI : "+model.getIMEI());
+                holder.password.setText("password : "+model.getPassword());
+                holder.mailED.setText("MailID : " +model.getMail());
+                holder.city.setText("City : " +model.getCity());
+                holder.phone.setText("Phone : " +model.getPhone());
+                holder.profile.setText("profile : " + "Employee");
+
+                holder.delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        CharSequence options[] = new CharSequence[]{
+                                "Yes",
+                                "No"
+                        };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Employee_Active_user.this);
+                        builder.setTitle("Sure want to Delete this Employee profile ?");
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                if(i==0){
+                                    String uID = getRef(position).getKey();
+                                    RemoveEmp(uID);
+
+                                }else{
+
+                                }
+
+                            }
+                        });
+                        builder.show();
+                    }
+                });
+
+
+            }
+
+
+            @NonNull
+
+            @Override
+            public ActiveUserActivity.EmplistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.emp_display_layout,parent,false);
+                return new ActiveUserActivity.EmplistViewHolder(view);
+            }
+        };
+
+        list.setAdapter(empadapter);
+        empadapter.startListening();
+    }
+
+    public void EmployeeFirebasesearch() {
+
+        String s = inputtext.getText().toString();
+        Query query = empref.orderByChild("Name").startAt(s);
+
+        empref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                count = (int) snapshot.getChildrenCount();
+                text.setText("count :"+count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        FirebaseRecyclerOptions<Employee> empoptions =
+                new FirebaseRecyclerOptions.Builder<Employee>()
+                        .setQuery(query, Employee.class)
                         .build();
 
         FirebaseRecyclerAdapter<Employee, ActiveUserActivity.EmplistViewHolder> empadapter = new FirebaseRecyclerAdapter<Employee, ActiveUserActivity.EmplistViewHolder>(empoptions) {
