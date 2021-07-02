49c05c83f9a8f76079fcfe9fa8c2724dc2426cd4
Priyanshu132 <priyanshugupta132@gmail.com>
2021-07-02 19:12:24 +0530

    2_july_evening

--- app/src/main/java/com/crm/pvt/hapinicrm/Add_new_employee_activity.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/Add_new_employee_activity.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +46,9 @@ public class Add_new_employee_activity extends AppCompatActivity {
     private TextInputEditText conf_pass;
     private ProgressDialog loadingBar;
     private EditText Empname,CityE,phoneE;
+    private Spinner spinner;
+    String[] type = {"candidate","vendors","customerb2c","customerb2b"};
+    String type_of_emp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +59,18 @@ public class Add_new_employee_activity extends AppCompatActivity {
         if(new InternetDialog(getApplicationContext()).getInternetStatus()){
             //   Toast.makeText(getContext(), "INTERNET VALIDATION PASSED", Toast.LENGTH_SHORT).show();
         }
+        spinner = findViewById(R.id.spinner_login);
+       spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               type_of_emp = spinner.getSelectedItem().toString();
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> adapterView) {
+
+           }
+       });
 
         build_number = findViewById(R.id.bulid_number);
         email = findViewById(R.id.Email);
@@ -100,8 +118,10 @@ public class Add_new_employee_activity extends AppCompatActivity {
 
         // get text
 
-
-        if(password.isEmpty()){
+        if(type_of_emp.contentEquals("Select Employee Type")){
+            ((TextView)spinner.getSelectedView()).setError("Choose Valid Type");
+        }
+        else if(password.isEmpty()){
             pass.setError("passcode can't be empty");
         }
         else if(empname.isEmpty()){
@@ -138,9 +158,6 @@ public class Add_new_employee_activity extends AppCompatActivity {
             CityE.requestFocus();
             CityE.setError("Enter valid name");
         }
-
-
-
         else if(IMEI.length() != 15){
 
             build_number.setError("Number should be of 15 digits");
@@ -150,33 +167,24 @@ public class Add_new_employee_activity extends AppCompatActivity {
             email.setError("Enter a valid email address");
             email.requestFocus();
         }
-
-
         else if(!password.equals(repassword))
         {
 
             conf_pass.setError("password and Confrim password should be same");
 
         }
-
-
         else if(!checkBox.isChecked())
         {
             checkBox.setError("Please check the box");
 
         }
-
-
-
-
-
         else{
             validatePass(password);
             loadingBar.setTitle("Create Account");
             loadingBar.setMessage("please Wait while checking Credentials..");
             loadingBar.setCanceledOnTouchOutside(false);
             loadingBar.show();
-            ValidateEmp(IMEI,mail,password,empname,cityE,PhoneE,"null");
+            ValidateEmp(type_of_emp,IMEI,mail,password,empname,cityE,PhoneE,"null");
 
 
 
@@ -186,7 +194,7 @@ public class Add_new_employee_activity extends AppCompatActivity {
 
     }
 
-    public void ValidateEmp(String IMEI,String mail,String password,String name,String city,String phone,String url){
+    public void ValidateEmp(String type,String IMEI,String mail,String password,String name,String city,String phone,String url){
 
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
@@ -202,7 +210,7 @@ public class Add_new_employee_activity extends AppCompatActivity {
                     EmpDataMap.put("Name",name);
                     EmpDataMap.put("City",city);
                     EmpDataMap.put("Phone",phone);
-
+                    EmpDataMap.put("Type",type);
                     EmpDataMap.put("ImgUrl",url);
 
 
