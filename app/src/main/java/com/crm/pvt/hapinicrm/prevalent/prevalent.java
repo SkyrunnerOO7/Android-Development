70e899623e1d23fe1a4a58dcaa2c487057589444
Rohan <rohankulk82@gmail.com>
2021-07-12 23:27:06 +0530

    Rohan 12_07_21 11:26pm

--- app/src/main/java/com/crm/pvt/hapinicrm/prevalent/prevalent.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/prevalent/prevalent.java
@@ -4,11 +4,14 @@ package com.crm.pvt.hapinicrm.prevalent;
 import com.crm.pvt.hapinicrm.models.Admin;
 import com.crm.pvt.hapinicrm.models.Attendance;
 import com.crm.pvt.hapinicrm.models.Employee;
+import com.crm.pvt.hapinicrm.models.Master;
 import com.google.firebase.database.DatabaseReference;
 
 public class prevalent {
     public static Employee CurrentOnlineEmloyee;
     public static Attendance CurrentUserAttendance;
+    public static Master CurrentMaster;
+
 
 
     public static Admin CurrentOnlineAdmin;
