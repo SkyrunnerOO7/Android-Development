29584e627b566ed4d2ede70b2ee1bd525054ce8c
Sanjana <kushwahasapna290220@gmail.com>
2021-07-17 21:57:53 +0530

    Sapna kushwaha 17 july 9:57 PM

--- app/src/main/java/com/crm/pvt/hapinicrm/CallReceiver.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/CallReceiver.java
@@ -7,23 +7,28 @@ import android.telephony.TelephonyManager;
 import android.view.Gravity;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
 //1 more comment
-            //2nd comment
-            //comment
-            //Toast.makeText(context," Receiver start ",Toast.LENGTH_SHORT).show();
 
             String state=intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
             int flag  =0;
             if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)){
-                showToast(context,"Call started...");
+                //showToast(context,"Call started...");
             }
             if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)){
                 showToast(context,"Idle Call ended...");
+
+
                 Intent i = new Intent(context, callingFeedbackActivity.class);
 
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -33,7 +38,7 @@ public class CallReceiver extends BroadcastReceiver {
 
             }
             if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)){
-                showToast(context,"Incoming call...");
+                //showToast(context,"Incoming call...");
                 flag = 1;
 
             }
